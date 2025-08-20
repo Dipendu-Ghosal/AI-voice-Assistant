686c748cc80d17a0cc09852958270140bd1c2388
Dipendu Ghosal <dipendughosal@gmail.com>
2025-08-20 12:13:49 +0530

    VS code error

--- main.py
+++ main.py
@@ -8,5 +8,21 @@ engine = pyttsx3.init()
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+
 if __name__ == "__main__":
-    speak("Hello there")
+    speak("Intializing Voice Assistant")
+    r = sr.Recognizer()
+    while True:
+        # Listen for Calling 
+        #obtain audio from mic
+        try:
+            with sr.Microphone() as source:
+                print("Listening!")
+            audio = r.listen(source)
+
+            command = r.recognize_google(audio)
+            print(command)
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
